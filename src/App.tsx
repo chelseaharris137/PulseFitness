366c4d025f4e880853126550ff549d88a4c2a3f6
chelseaharris137 <72273359+chelseaharris137@users.noreply.github.com>
2023-02-10 16:14:17 -0500

    Add navbar scene

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,32 @@
+import Navbar from '@/scenes/navbar';
+import { useEffect, useState } from "react";
+import { SelectedPage } from "@/shared/types";
+
 function App() {
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(
+    SelectedPage.Home
+  );
+  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setIsTopOfPage(true);
+        setSelectedPage(SelectedPage.Home);
+      }
+      if (window.scrollY !== 0) setIsTopOfPage(false);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
     <div className="app bg-gray-20">
-      Hello world!
+      <Navbar
+        isTopOfPage={isTopOfPage}
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
     </div>
   )
 }

c1825079cc96d6f833c3d18c9ece64b7a9fa6f5d
chelseaharris137 <72273359+chelseaharris137@users.noreply.github.com>
2023-02-13 15:07:43 -0500

    Add homepage

---
+++ src/scenes/home/index.tsx
@@ -0,0 +1,95 @@
+import useMediaQuery from "@/hooks/useMediaQuery";
+import { SelectedPage } from "@/shared/types";
+import ActionButton from "@/shared/ActionButton";
+import PulseText from "@/assets/images/PulseText.png";
+import HomePageGraphic from "@/assets/images/HomePageGraphic.png";
+import SponsorRedBull from "@/assets/images/SponsorRedBull.png";
+import SponsorForbes from "@/assets/images/SponsorForbes.png";
+import SponsorFortune from "@/assets/images/SponsorFortune.png";
+import AnchorLink from "react-anchor-link-smooth-scroll";
+import { motion } from "framer-motion";
+
+type Props = {
+  setSelectedPage: (value: SelectedPage) => void;
+};
+
+const Home = ({ setSelectedPage }: Props) => {
+  const isAboveMediumScreens = useMediaQuery("(min-width:1060px)");
+
+  return (
+    <section id="home" className="gap-16 bg-gray-20 py-10 md:h-full md:pb-0">
+      <motion.div
+        className="mx-auto w-5/6 items-center justify-center md:flex md:h-5/6"
+        onViewportEnter={() => setSelectedPage(SelectedPage.Home)}
+      >
+        <div className="z-10 mt-32 md:basis-3/5">
+          <motion.div
+            className="md:-mt-20"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ duration: 0.5 }}
+            variants={{
+              hidden: { opacity: 0, x: -50 },
+              visible: { opacity: 1, x: 0 },
+            }}
+          >
+            <div className="relative">
+              <div className="before:absolute before:-top-20 before:-left-20 before:z-[-1] md:before:content-evolvetext">
+                <img alt="PULSE Eat. Sleep. Gym. Repeat." src={PulseText} />
+              </div>
+            </div>
+
+            <p className="mt-8 text-sm">
+              Join us today and start your journey to a healthier, happier you! With our commitment to your success and a supportive community of like-minded individuals, there's no limit to what you can achieve.
+            </p>
+          </motion.div>
+
+          <motion.div
+            className="mt-8 flex items-center gap-8"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ delay: 0.2, duration: 0.5 }}
+            variants={{
+              hidden: { opacity: 0, x: -50 },
+              visible: { opacity: 1, x: 0 },
+            }}
+          >
+            <ActionButton setSelectedPage={setSelectedPage}>
+              Join Now
+            </ActionButton>
+            <AnchorLink
+              className="text-sm font-bold text-primary-500 underline hover:text-secondary-500"
+              onClick={() => setSelectedPage(SelectedPage.ContactUs)}
+              href={`#${SelectedPage.ContactUs}`}
+            >
+              <p>Learn More</p>
+            </AnchorLink>
+          </motion.div>
+        </div>
+
+        <div
+          className="flex basis-2/5 justify-center md:z-10
+              md:ml-40 md:mt-16 md:justify-items-end"
+        >
+          <img alt="home-pageGraphic" src={HomePageGraphic} />
+        </div>
+      </motion.div>
+
+      {isAboveMediumScreens && (
+        <div className="h-[150px] w-full bg-primary-300 py-10">
+          <div className="mx-auto w-5/6">
+            <div className="flex w-3/5 items-center justify-between gap-8">
+              <img alt="redbull-sponsor" src={SponsorRedBull} />
+              <img alt="forbes-sponsor" src={SponsorForbes} />
+              <img alt="fortune-sponsor" src={SponsorFortune} />
+            </div>
+          </div>
+        </div>
+      )}
+    </section>
+  );
+};
+
+export default Home;

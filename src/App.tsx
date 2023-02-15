4ce91b29fdb5bb124b2146020d737d42d1b1445d
chelseaharris137 <72273359+chelseaharris137@users.noreply.github.com>
2023-02-15 13:02:33 -0500

    Add classes scene

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Benefits from "@/scenes/benefits";
 import Home from "@/scenes/home";
 import Navbar from '@/scenes/navbar';
+import OurClasses from "@/scenes/ourClasses";
 import { useEffect, useState } from "react";
 import { SelectedPage } from "@/shared/types";
 
@@ -32,6 +33,7 @@ function App() {
 
       <Home setSelectedPage={setSelectedPage} />
       <Benefits setSelectedPage={setSelectedPage} />
+      <OurClasses setSelectedPage={setSelectedPage} />
     </div>
   )
 }
--- src/shared/types.ts
+++ src/shared/types.ts
@@ -10,3 +10,9 @@ export interface BenefitType {
   title: string;
   description: string;
 }
+
+export interface ClassType {
+  name: string;
+  description?: string;
+  image: string;
+}
